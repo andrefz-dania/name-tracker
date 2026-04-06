ca46394790b49447bcbbd19593247a8357277136
andrefz-dania <116558693+andrefz-dania@users.noreply.github.com>
2026-04-06 18:13:13 +0200

    add images to expanded list

--- src/renderer/src/components/AvatarLoader.svelte
+++ src/renderer/src/components/AvatarLoader.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import { CircleUserRound } from '@lucide/svelte'
-  import { onMount } from 'svelte'
 
   let { id }: { id: number } = $props()
   let generatedUrl = $state('')
+    let loaded = $state(false)
 
   async function loadImage(id: number) {
+    loaded = false
     const buffer = await window.api.loadImage(id)
-    if (buffer.image.byteLength > 1) {
+    if (buffer && buffer.image && buffer.image.byteLength > 1) {
       const blob = new Blob([buffer.image], { type: 'image/jpeg' })
       const imageUrl = URL.createObjectURL(blob)
       generatedUrl = imageUrl
@@ -15,16 +16,18 @@
     }
   }
 
-  let loaded = $state(false)
-  onMount(() => {
+  $effect(()=>{
     loadImage(id)
-  })
+  }) 
+
+
 </script>
 
 <div class="max-w-44 aspect-square border-3 border-primary rounded-full overflow-hidden flex items-center">
     {#if loaded}
     <img alt="" src={generatedUrl} class="rounded-full"/>
   {:else}
-    <CircleUserRound size={176} class="text-primary"></CircleUserRound>
+
+    <CircleUserRound class="text-primary w-full" size={'auto'}></CircleUserRound>
   {/if}
 </div>
