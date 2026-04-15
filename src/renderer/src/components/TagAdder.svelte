64c26f49167dbe45f4beaf1ef10e0fdd4af97128
andrefz-dania <116558693+andrefz-dania@users.noreply.github.com>
2026-04-15 02:29:04 +0200

    fix tag menu display

--- src/renderer/src/components/TagAdder.svelte
+++ src/renderer/src/components/TagAdder.svelte
@@ -41,13 +41,13 @@
   <div class="relative h-0 w-0">
     {#if open}
       <div
-        class="absolute shadow-md bg-layer0 border border-primary min-w-64 top-8 -left-8 p-2 flex flex-col gap-2 rounded-md max-h-64"
+        class="absolute shadow-md bg-layer0 border border-primary min-w-64 top-8 -left-8 p-2 flex flex-col gap-2 rounded-md max-h-96"
       >
         <div class="flex flex-row gap-2 place-content-between items-center min-w-full">
           <p class="text-primary-highlight pl-3 text-lg font-bold">Select tags to add</p>
           <ButtonDecorated style="transparent" type='button' onclick={() => open = false}><XIcon></XIcon></ButtonDecorated>
         </div>
-        <div class="flex flex-row gap-2 flex-wrap w-full p-2">
+        <div class="flex flex-row gap-2 flex-wrap w-full p-2 overflow-y-scroll">
           {#each allTags as t}
             <button
               type="button"
